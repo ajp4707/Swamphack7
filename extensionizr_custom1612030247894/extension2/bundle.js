90ffe821917e6af548921253f2fdb84418630cf8
ajp4707 <52212289+ajp4707@users.noreply.github.com>
2021-01-31 02:51:16 -0500

    Final edits

--- extensionizr_custom1612030247894/extension2/bundle.js
+++ extensionizr_custom1612030247894/extension2/bundle.js
@@ -14,7 +14,6 @@ function shuffle(array) {
     array[currentIndex] = array[randomIndex];
     array[randomIndex] = temporaryValue;
   }
-
   return array;
 }
 
@@ -25,14 +24,15 @@ var posTagger = require( 'wink-pos-tagger' );
 var tagger = posTagger();
 
 var elements = document.getElementsByTagName('*');
-console.log("Array of elems looks like this");
-console.log(elements);
-var nounlist = [];
 
-var nodelist = [];
+var posDict = {"NN":[[],[],[]], "NNP":[[],[],[]], "NNS":[[],[],[]], "JJ":[[],[],[]]}; 	//Dictionaries to hold words and corresponding nodes
+var keys = ["NN", "NNP", "NNS", "JJ"];
+
 var goodElemsList = ["BODY", "DIV", "P", "A", "LABEL", "BUTTON", "H1", "H2", "H3", "H4", "H5", "H6", "UL", "OL", "LI", "NAV", "FOOTER", "HEADER" ];
 //First loop finds all the nouns and adds to nounlist
+var MAXCOUNT = 500;
 
+var count = 0;
 for (var i = 0; i < elements.length; i++)
 	if(elements[i].nodeType === 1 && goodElemsList.includes(elements[i].nodeName))
 	for (var j = 0; j < elements[i].childNodes.length; j++) {
@@ -40,33 +40,33 @@ for (var i = 0; i < elements.length; i++)
 	
 	    if (node.nodeType === 3 && node.nodeValue.trim()) {
 	        var text = node.nodeValue.trim();
-	        
 	        var arr = tagger.tagSentence(text);
-	        console.log("Tagger found:")
-	        console.log(arr);
-	        for (var k = 0; k < arr.length && nounlist.length <= 300; k++){
-	        	if (arr[k].pos === "NN" || arr[k].pos === "NNP" || arr[k].pos === "NNS"){
-	        		nounlist.push(arr[k].value);
-	        		nodelist.push(node);
-	        	}
+	        for (var k = 0; k < arr.length && count <= MAXCOUNT; k++){
+	        	for (var z = 0; z < keys.length; z++)
+		        	if (arr[k].pos === keys[z]){
+		        		posDict[keys[z]][0].push(arr[k].value);
+		        		posDict[keys[z]][1].push(node);
+		        		posDict[keys[z]][2].push(arr[k].value);
+		        		count++;
+		        	}
 	        }
 	    }
 	}
-console.log("Nounlist looks like:")
-console.log(nounlist);
-var nounlistshuffled = [];
-for (var i = 0; i < nounlist.length; i++)
-	nounlistshuffled.push(nounlist[i]);
-shuffle(nounlistshuffled);
-
-console.log("Replacing elems now");
-for (var i = 0; i < nounlist.length; i++)
-{
-	var node2 = nodelist[i];
-	var text = node2.nodeValue;
-	var replacedText = text.replace(nounlist[i], nounlistshuffled[i]);
-	if (replacedText !== text) 
-        node2.replaceWith(document.createTextNode(replacedText));
+for (var i = 0; i < keys.length; i++){
+	var multiArr = posDict[keys[i]];
+	shuffle(multiArr[2]);
+	console.log(multiArr[0].length);
+	console.log("Replacing elems now");
+	for (var j = 0; j < multiArr[0].length; j++)
+	{
+		var node2 = multiArr[1][j];
+		var text = node2.nodeValue;
+		var replacedText = text.replace(multiArr[0][j],  multiArr[2][j].toUpperCase());
+		
+		if (replacedText !== text) {
+	        node2.replaceWith(document.createTextNode(replacedText));
+		}
+	}
 }
 },{"wink-pos-tagger":21}],2:[function(require,module,exports){
 "use strict";
