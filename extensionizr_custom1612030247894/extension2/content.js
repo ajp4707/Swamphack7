c297464c5ae1999775562d3bee5f826a47204f86
ajp4707 <52212289+ajp4707@users.noreply.github.com>
2021-01-30 23:41:27 -0500

    Fix banana

--- extensionizr_custom1612030247894/extension2/content.js
+++ extensionizr_custom1612030247894/extension2/content.js
@@ -24,52 +24,46 @@ var posTagger = require( 'wink-pos-tagger' );
 var tagger = posTagger();
 
 var elements = document.getElementsByTagName('*');
-
+console.log("Array of elems looks like this");
+console.log(elements);
 var nounlist = [];
 
+var nodelist = [];
+var goodElemsList = ["BODY", "DIV", "P", "A", "LABEL", "BUTTON", "H1", "H2", "H3", "H4", "H5", "H6", "UL", "OL", "LI", "NAV", "FOOTER", "HEADER" ];
 //First loop finds all the nouns and adds to nounlist
-for (var i = 0; i < elements.length; i++) {
-    var element = elements[i];
-    if (element.nodeType === 1)
-	    for (var j = 0; j < element.childNodes.length; j++) {
-	        var node = element.childNodes[j];
-	
-	        if (node.nodeType === 3) {
-	            var text = node.nodeValue;
-	            
-	            var arr = tagger.tagSentence(text);
-	            
-	           for (var k = 0; k < arr.length && nounlist.length <= 50; k++){
-	            	if (arr[k].pos === 'NN')
-	            		nounlist.push(arr[k].value);
-	            }
-        }
-    }
-}
 
-var nounlistcopy = ['Dummy'];
+for (var i = 0; i < elements.length; i++)
+	if(elements[i].nodeType === 1 && goodElemsList.includes(elements[i].nodeName))
+	for (var j = 0; j < elements[i].childNodes.length; j++) {
+	    var node = elements[i].childNodes[j];
+	
+	    if (node.nodeType === 3 && node.nodeValue.trim()) {
+	        var text = node.nodeValue.trim();
+	        
+	        var arr = tagger.tagSentence(text);
+	        console.log("Tagger found:")
+	        console.log(arr);
+	        for (var k = 0; k < arr.length && nounlist.length <= 300; k++){
+	        	if (arr[k].pos === "NN" || arr[k].pos === "NNP" ||arr[k].pos === "NNS"){
+	        		nounlist.push(arr[k].value);
+	        		nodelist.push(node);
+	        	}
+	        }
+	    }
+	}
+console.log("Nounlist looks like:")
+console.log(nounlist);
+var nounlistshuffled = [];
 for (var i = 0; i < nounlist.length; i++)
-	nounlistcopy.push(nounlist);
-shuffle(nounlist);
+	nounlistshuffled.push(nounlist[i]);
+shuffle(nounlistshuffled);
 
-//Second loop replaces the nouns
-for (var i = 0; i < elements.length; i++) {
-    var element = elements[i];
-
-    for (var j = 0; j < element.childNodes.length; j++) {
-        var node = element.childNodes[j];
-
-        if (node.nodeType === 3) {
-        	for (var k = 0; k < nounlist.length; k++){
-	            var text = node.nodeValue;
-	            
-	            var replacedText = text.replace(nounlist[k], nounlistcopy[k]);
-	
-	            if (replacedText !== text) {
-	                //element.replaceChild(document.createTextNode(replacedText), node);
-	                node.replaceWith(document.createTextNode(replacedText));
-	            	}
-            }
-        }
-    }
+console.log("Replacing elems now");
+for (var i = 0; i < nounlist.length; i++)
+{
+	var node2 = nodelist[i];
+	var text = node2.nodeValue;
+	var replacedText = text.replace(nounlist[i], nounlistshuffled[i]);
+	if (replacedText !== text) 
+        node2.replaceWith(document.createTextNode(replacedText));
 }
